b9ba9d129fe4276b5bb820dd52a6a3fd38a77063
suryansh55 <36988384+suryansh55@users.noreply.github.com>
2026-03-07 11:40:17 -0700

    added profile page and instrumental test for it (#53)

---
+++ app/src/androidTest/java/com/example/vigilante/ProfileTest.java
@@ -0,0 +1,107 @@
+package com.example.vigilante;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.clearText;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.RootMatchers.withDecorView;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.is;
+
+import static java.util.function.Predicate.not;
+
+import android.widget.EditText;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+
+import com.google.android.gms.tasks.Tasks;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.hamcrest.Matchers;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.util.regex.Matcher;
+
+public class ProfileTest {
+
+    @Rule
+    public ActivityScenarioRule<ProfilePage> activityRule =
+            new ActivityScenarioRule<>(ProfilePage.class);
+
+    @Before
+    public void setUp() throws Exception {
+
+        FirebaseAuth.getInstance().signOut();
+
+        Tasks.await(FirebaseAuth.getInstance().signInWithEmailAndPassword("ash@test.com", "ash123"));
+
+        ActivityScenario.launch(ProfilePage.class);
+
+
+        Thread.sleep(1500);
+    }
+
+    @Test
+    public void testProfileDataLoads() {
+        // Wait for Firestore (approx 2 seconds)
+        try { Thread.sleep(2000); } catch (InterruptedException e) {}
+
+
+        onView(withId(R.id.name_text)).check(matches(withText("Bash")));
+
+        // Check if Email is displayed correctly
+        onView(withId(R.id.email_text)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testUpdatePhoneDialog(){
+        onView(withId(R.id.UpdateInfo_button)).perform(click());
+
+        onView(withText("Phone")).perform(click());
+
+        onView(withClassName(Matchers.equalTo(EditText.class.getName()))).perform(clearText(),typeText("09345678456"));
+
+        try { Thread.sleep(3000);} catch (InterruptedException e) {}
+
+        onView(withText("Save")).perform(click());
+
+        try { Thread.sleep(3000);} catch (InterruptedException e) {}
+
+        onView(withId(R.id.phonenumber_text)).check(matches(withText("09345678456")));
+    }
+
+    @Test
+    public void testEmailUpdateSendsVerification() {
+        onView(withId(R.id.UpdateInfo_button)).perform(click());
+        onView(withText("Email")).perform(click());
+
+        String newEmail = "new_test@test.com";
+
+        onView(withClassName(Matchers.equalTo(EditText.class.getName()))).perform(clearText(),typeText(newEmail));
+
+        onView(withId(android.R.id.button1)).perform(click());
+
+        try { Thread.sleep(3000);} catch (InterruptedException e) {}
+
+        onView(withText("Update Email")).check(doesNotExist());
+
+        onView(withId(R.id.email_text)).check(matches(withText(newEmail)));
+
+    }
+
+    @After
+    public void tearDown() {
+        FirebaseAuth.getInstance().signOut();
+    }
+}
