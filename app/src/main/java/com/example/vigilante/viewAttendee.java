ad87824bb735a99f1e6ae30a642650978b8cea27
suryansh55 <suryanshankur55@gmail.com>
2026-03-12 16:28:38 -0600

    added a way to view signed up attendees

---
+++ app/src/main/java/com/example/vigilante/viewAttendee.java
@@ -0,0 +1,56 @@
+package com.example.vigilante;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.CalendarContract;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class viewAttendee extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+
+    private ProfileAdapter attendeeAdapter;
+    private List<Profile> attendeeList;
+    private FirebaseFirestore db;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.attendee_list);
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        recyclerView = findViewById(R.id.attendees_recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        attendeeList = new ArrayList<>();
+        Button back_button = (Button) findViewById(R.id.back_button);
+        attendeeAdapter = new ProfileAdapter(attendeeList);
+        recyclerView.setAdapter(attendeeAdapter);
+        String eventId = getIntent().getStringExtra("EVENT_ID");
+
+        db.collection("events").document(eventId).collection("attendees").get().addOnSuccessListener(queryDocumentSnapshots -> {
+            attendeeList.clear();
+            for (QueryDocumentSnapshot document : queryDocumentSnapshots) {
+                Profile attendee = document.toObject(Profile.class);
+                attendeeList.add(attendee);
+
+            }
+            attendeeAdapter.notifyDataSetChanged();
+        });
+
+        back_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
